617d1bbddfff82f75c979e5f24027fd3777209f2
Albertofran <111243166+AlbertoBfran@users.noreply.github.com>
2024-08-26 21:48:13 -0500

    controllers

---
+++ src/main/java/pe/edu/upc/taytagrupo5/controllers/UserController.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.taytagrupo5.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.taytagrupo5.dtos.UserDTO;
+import pe.edu.upc.taytagrupo5.entities.User;
+import pe.edu.upc.taytagrupo5.serviceinterfaces.IUserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserService uS;
+
+    @GetMapping
+    public List<UserDTO> listar(){
+        return uS.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x, UserDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
