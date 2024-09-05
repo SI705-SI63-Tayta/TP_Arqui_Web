43d12bcee340c168f30265e1ad308bcc42f291df
RenzoLenes <lenesrenzoalberto@gmail.com>
2024-09-05 01:06:12 -0500

    relations review-user and user-appointment

--- src/main/java/pe/edu/upc/taytagrupo5/entities/Appointment.java
+++ src/main/java/pe/edu/upc/taytagrupo5/entities/Appointment.java
@@ -2,7 +2,7 @@ package pe.edu.upc.taytagrupo5.entities;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="Appointments")
@@ -12,7 +12,7 @@ public class Appointment {
     private int idAppointment;
 
     @Column(name = "date", nullable = false)
-    private Date date;
+    private LocalDate date;
 
     @Column(name = "reason", nullable = false, length = 30)
     private String reason;
@@ -23,15 +23,25 @@ public class Appointment {
     @Column(name = "description", nullable = false, length = 50)
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "idCliente")
+    private User userCliente;
+
+    @ManyToOne
+    @JoinColumn(name = "idPersonal")
+    private User userPersonal;
+
     public Appointment() {
     }
 
-    public Appointment(int idAppointment, Date date, String reason, String mode, String description) {
+    public Appointment(int idAppointment, LocalDate date, String reason, String mode, String description, User userCliente, User userPersonal) {
         this.idAppointment = idAppointment;
         this.date = date;
         this.reason = reason;
         this.mode = mode;
         this.description = description;
+        this.userCliente = userCliente;
+        this.userPersonal = userPersonal;
     }
 
     public int getIdAppointment() {
@@ -42,11 +52,11 @@ public class Appointment {
         this.idAppointment = idAppointment;
     }
 
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
@@ -72,5 +82,21 @@ public class Appointment {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public User getUserCliente() {
+        return userCliente;
+    }
+
+    public void setUserCliente(User userCliente) {
+        this.userCliente = userCliente;
+    }
+
+    public User getUserPersonal() {
+        return userPersonal;
+    }
+
+    public void setUserPersonal(User userPersonal) {
+        this.userPersonal = userPersonal;
     }
 }
